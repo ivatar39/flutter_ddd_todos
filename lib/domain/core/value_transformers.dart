cf4e98cf148a4ef554cfd738dd7f31e4d505e8b8
ivlup <ivlupsecond@gmail.com>
2020-11-09 21:36:47 +0200

    [12] - Note Value Objects

---
+++ lib/domain/core/value_transformers.dart
@@ -0,0 +1,5 @@
+import 'package:flutter/material.dart';
+
+Color makeColorOpaque(Color color) {
+  return color.withOpacity(1);
+}
--- lib/domain/core/value_validators.dart
+++ lib/domain/core/value_validators.dart
@@ -1,5 +1,54 @@
 import 'package:dartz/dartz.dart';
+import 'package:kt_dart/kt.dart';
 import 'failures.dart';
+
+Either<ValueFailure<String>, String> validateMaxStringLength(
+  String input,
+  int maxLength,
+) {
+  if (input.length <= maxLength) {
+    return right(input);
+  } else {
+    return left(ValueFailure.exceedingLength(
+      failedValue: input,
+      max: maxLength,
+    ));
+  }
+}
+
+Either<ValueFailure<String>, String> validateStringNotEmpty(String input) {
+  if (input.isNotEmpty) {
+    return right(input);
+  } else {
+    return left(ValueFailure.empty(
+      failedValue: input,
+    ));
+  }
+}
+
+Either<ValueFailure<String>, String> validateSingleLine(String input) {
+  if (!input.contains('\n')) {
+    return right(input);
+  } else {
+    return left(ValueFailure.multiline(
+      failedValue: input,
+    ));
+  }
+}
+
+Either<ValueFailure<KtList<T>>, KtList<T>> validateMaxListLength<T>(
+  KtList<T> input,
+  int maxLength,
+) {
+  if (input.size <= maxLength) {
+    return right(input);
+  } else {
+    return left(ValueFailure.listTooLong(
+      failedValue: input,
+      max: maxLength,
+    ));
+  }
+}
 
 Either<ValueFailure<String>, String> validateEmailAddress(String input) {
   const emailRegex =
