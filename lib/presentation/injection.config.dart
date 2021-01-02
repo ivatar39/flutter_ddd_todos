a7af43f7acc3f4d3509aaee195288f09f8dcee84
Ivan Taranov <37039674+ivatar39@users.noreply.github.com>
2021-01-03 00:42:11 +0200

    [18] - Note Form Bloc

--- lib/presentation/injection.config.dart
+++ lib/presentation/injection.config.dart
@@ -16,6 +16,7 @@ import '../infrastructure/core/firebase_injectable_module.dart';
 import '../domain/auth/i_auth_facade.dart';
 import '../domain/notes/i_note_repository.dart';
 import '../application/notes/note_actor/note_actor_bloc.dart';
+import '../application/notes/note_form/note_form_bloc.dart';
 import '../infrastructure/notes/note_repository.dart';
 import '../application/notes/note_watcher/note_watcher_bloc.dart';
 import '../application/auth/sign_in_form/sign_in_form_bloc.dart';
@@ -37,6 +38,7 @@ GetIt $initGetIt(
   gh.lazySingleton<INoteRepository>(
       () => NoteRepository(get<FirebaseFirestore>()));
   gh.factory<NoteActorBloc>(() => NoteActorBloc(get<INoteRepository>()));
+  gh.factory<NoteFormBloc>(() => NoteFormBloc(get<INoteRepository>()));
   gh.factory<NoteWatcherBloc>(() => NoteWatcherBloc(get<INoteRepository>()));
   gh.factory<SignInFormBloc>(() => SignInFormBloc(get<IAuthFacade>()));
   gh.factory<AuthBloc>(() => AuthBloc(get<IAuthFacade>()));
---
+++ lib/presentation/notes/note_form/misc/todo_item_presentation_classes.dart
@@ -0,0 +1,39 @@
+import 'package:flutter_ddd_todos/domain/auth/value_objects.dart';
+import 'package:flutter_ddd_todos/domain/notes/todo_item.dart';
+import 'package:flutter_ddd_todos/domain/notes/value_objects.dart';
+import 'package:freezed_annotation/freezed_annotation.dart';
+
+part 'todo_item_presentation_classes.freezed.dart';
+
+@freezed
+abstract class TodoItemPrimitive implements _$TodoItemPrimitive {
+  const TodoItemPrimitive._();
+
+  const factory TodoItemPrimitive({
+    @required UniqueId id,
+    @required String name,
+    @required bool done,
+  }) = _TodoItemPrimitive;
+
+  factory TodoItemPrimitive.empty() => TodoItemPrimitive(
+        id: UniqueId(),
+        name: '',
+        done: false,
+      );
+
+  factory TodoItemPrimitive.fromDomain(TodoItem todoItem) {
+    return TodoItemPrimitive(
+      id: todoItem.id,
+      name: todoItem.name.getOrCrash(),
+      done: todoItem.done,
+    );
+  }
+
+  TodoItem toDomain() {
+    return TodoItem(
+      id: id,
+      name: TodoName(name),
+      done: done,
+    );
+  }
+}
